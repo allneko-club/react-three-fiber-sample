0ebdfc7fcb2c397ecc4a9f204e9771cfc8589d95
Tanahashi <contrapunctus1to12@gmail.com>
2024-05-16 15:16:45 +0900

    画像追加 README.md更新

--- README.md
+++ README.md
@@ -2,6 +2,12 @@
 
 react(next.js), three.js, React Three Fiberを使ったglTFビュアーのサンプル
 
+画面上で拡大縮小、ズームインズームアウト、カメラの移動ができます
+
+右上のコントローラーを操作することでライトの調整や環境の変更などができます
+
+![Viewer Image 1](sample/image/01.png)
+
 ## 開始方法
 
 ```bash
--- components/layout/Footer.js
+++ components/layout/Footer.js
@@ -1,19 +1,9 @@
 
-function Copyright() {
-  return (
-    <p className="m-0 text-center">
-      {'© 2022-'}
-      {new Date().getFullYear()}
-      {' Allneko Club Inc.'}
-    </p>
-  );
-}
-
 export function Footer() {
   return (
     <footer className="mt-auto py-3 bg-dark">
       <div className="container">
-        <Copyright />
+        Three.js React Viewer
       </div>
     </footer>
   );
